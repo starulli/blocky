9181821966012d61aad90489f35aad0b072acebb
Stephan Tarulli <srt@tinychameleon.com>
2020-01-20 20:46:24 -0800

    Add image loading for SVG generation

--- svg/svg.go
+++ svg/svg.go
@@ -4,12 +4,37 @@ package svg
 
 import (
 	"fmt"
+	"image"
+	"io"
+	"os"
+
+	_ "image/png"
 )
 
 // New creates an instance of svg.Interface to convert an image
 // into an SVG file.
-func New(srcPath string) (Interface, error) {
-	return svg{}, nil
+func New(srcFilePath string) (Interface, error) {
+	file, err := os.Open(srcFilePath)
+	if err != nil {
+		return nil, svgError(err)
+	}
+	defer file.Close()
+
+	m, err := loadImage(file)
+	if err != nil {
+		return nil, svgError(err)
+	}
+
+	return svg{m: m}, nil
+}
+
+func loadImage(r io.Reader) (image.Image, error) {
+	m, _, err := image.Decode(r)
+	return m, err
+}
+
+func svgError(e error) error {
+	return fmt.Errorf("svg error: %w", e)
 }
 
 // Interface exists to explicitly define what can be done to SVG
@@ -19,6 +44,7 @@ type Interface interface {
 }
 
 type svg struct {
+	m image.Image
 }
 
 func (s svg) String() string {
--- svg/svg_test.go
+++ svg/svg_test.go
@@ -1,6 +1,11 @@
 package svg
 
-import "testing"
+import (
+	"encoding/base64"
+	"io"
+	"strings"
+	"testing"
+)
 
 func TestSvgStringerOutput(t *testing.T) {
 	s := svg{}
@@ -10,3 +15,38 @@ func TestSvgStringerOutput(t *testing.T) {
 		t.Errorf("Got %v, want %v", got, want)
 	}
 }
+
+func TestLoadImage(t *testing.T) {
+	m, _ := loadImage(imageReader())
+	width, height := 9, 9
+
+	if m.Bounds().Max.X != width {
+		t.Errorf("Got width %v, want %v", m.Bounds().Max.X, width)
+	}
+
+	if m.Bounds().Max.Y != height {
+		t.Errorf("Got height %v, want %v", m.Bounds().Max.Y, height)
+	}
+
+	m, err := loadImage(badImageReader())
+	if err == nil {
+		t.Errorf("Expected error for bad image")
+	}
+}
+
+func imageReader() io.Reader {
+	return base64.NewDecoder(base64.StdEncoding,
+		strings.NewReader(imageData))
+}
+
+func badImageReader() io.Reader {
+	badData := imageData[9:]
+	return base64.NewDecoder(base64.StdEncoding,
+		strings.NewReader(badData))
+}
+
+const imageData = `
+iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJAgMAAACd/+6DAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAL
+EwAACxMBAJqcGAAAAAxQTFRF4Ts7O57hmzvhAAAAgavRowAAAAR0Uk5T////AEAqqfQAAAAiSURB
+VHicYxANZWBwaHBgKGh2YKjpd2DIWgWhQXyQOFAeAKXYCSUY0KxtAAAAAElFTkSuQmCC
+`
